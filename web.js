990a180f6b33d78a91dc622c746878d1aa56683a
florentplomb <plombf@gmail.com>
2016-04-18 11:08:14 +0200

    test du port herkou

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var server = app.listen(process.env.PORT || 3000);
 var io = require('socket.io').listen(server);
 
 app.use(express.static(__dirname + '/app'));
-console.log("Listening on")
+console.log("Listening on " + process.env.PORT)
 io.sockets.on('connection', function (socket) {
     console.log('Un client est connecté !');
 });
